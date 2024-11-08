81a987c20f1a6e8e467177a797ae5fd48d470c71
Jorge <jorgecornejoa10@gmail.com>
2024-11-08 11:10:48 -0700

    act login v2

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4201874689"
+    serviceWorkerVersion: "2651023960"
   }
 });
